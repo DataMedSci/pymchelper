3db5e9ed120db6e755ab15afaa14cc9a5731382b
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-07-21 18:18:46 +0200

    obsolete fiels removed

--- tests/res/shieldhit/generated/generate_bdo.sh
+++ tests/res/shieldhit/generated/generate_bdo.sh
@@ -4,11 +4,11 @@ WORKDIR=tests/res/shieldhit/generated
 PYTHONPATH=. python $WORKDIR/generate_detect.py $WORKDIR
 for EST in "cyl" "geomap" "msh" "plane" "zone"
 do
-  echo $EST
-  echo $WORKDIR
   mkdir -p $WORKDIR/$EST
   cp ${WORKDIR}/mat.dat ${WORKDIR}/${EST}/
   cp ${WORKDIR}/beam.dat ${WORKDIR}/${EST}/
   cp ${WORKDIR}/geo.dat ${WORKDIR}/${EST}/
   shieldhit -n1000 -d ${WORKDIR}/detect_${EST}.dat ${WORKDIR}/${EST}
+  rm ${WORKDIR}/${EST}/for*
+  rm ${WORKDIR}/${EST}/*.dat
 done
