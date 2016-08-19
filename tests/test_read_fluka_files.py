aec5a166c9cf41751f3cfa3440a3ad71b3ec58c6
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-08-19 09:08:30 +0200

    better tests

--- tests/test_read_fluka_files.py
+++ tests/test_read_fluka_files.py
@@ -2,7 +2,7 @@ import os
 import unittest
 import logging
 
-from pymchelper.flair.Data import Usrbin, unpackArray, Usrbdx, Resnuclei, Usrxxx
+# from pymchelper.flair.Data import Usrbin, unpackArray, Usrbdx, Resnuclei, Usrxxx
 import pymchelper.flair.Input as Input
 
 logger = logging.getLogger(__name__)
@@ -11,79 +11,82 @@ logger = logging.getLogger(__name__)
 class TestDefaultConverter(unittest.TestCase):
     main_dir = os.path.join("tests", "res", "fluka")
 
-    def read_data(self, usr):
-        for i in range(len(usr.detector)):
-            logger.debug("-" * 20 + (" Detector number %i " % i) + "-" * 20)
-            usr.say(i)  # details for each detector
-        data = usr.readData(0)
-        fdata = unpackArray(data)
-        print(len(fdata), fdata[0:10])
+    # def read_data(self, usr):
+    #     for i in range(len(usr.detector)):
+    #         logger.debug("-" * 20 + (" Detector number %i " % i) + "-" * 20)
+    #         usr.say(i)  # details for each detector
+    #     data = usr.readData(0)
+    #     fdata = unpackArray(data)
+    #     print(len(fdata), fdata[0:10])
 
-    def check(self, rel_path):
-        try:
-            usr = Usrbin(rel_path)
-            usr.sayHeader()  # file,title,time,weight,ncase,nbatch
-            usr.say()  # file,title,time,weight,ncase,nbatch
-            self.read_data(usr)
-            print("!!! File ", rel_path, " is Usrbin")
-            return
-        except Exception as e:
-            print(e)
-            print("File ", rel_path, " is not Usrbin")
+    # def check(self, rel_path):
+    #     try:
+    #         usr = Usrbin(rel_path)
+    #         usr.sayHeader()  # file,title,time,weight,ncase,nbatch
+    #         usr.say()  # file,title,time,weight,ncase,nbatch
+    #         self.read_data(usr)
+    #         print("!!! File ", rel_path, " is Usrbin")
+    #         return
+    #     except Exception as e:
+    #         print(e)
+    #         print("File ", rel_path, " is not Usrbin")
+    #
+    #     try:
+    #         usr = Usrbdx(rel_path)
+    #         usr.sayHeader()  # file,title,time,weight,ncase,nbatch
+    #         usr.say()  # file,title,time,weight,ncase,nbatch
+    #         self.read_data(usr)
+    #         print("!!! File ", rel_path, " is Usrbdx")
+    #         return
+    #     except Exception as e:
+    #         print(e)
+    #         print("File ", rel_path, " is not Usrbdx")
+    #
+    #     try:
+    #         usr = Resnuclei(rel_path)
+    #         usr.sayHeader()  # file,title,time,weight,ncase,nbatch
+    #         usr.say()  # file,title,time,weight,ncase,nbatch
+    #         self.read_data(usr)
+    #         print("!!! File ", rel_path, " is Resnuclei")
+    #         return
+    #     except Exception as e:
+    #         print(e)
+    #         print("File ", rel_path, " is not Resnuclei")
+    #
+    #     try:
+    #         usr = Usrxxx(rel_path)
+    #         usr.sayHeader()  # file,title,time,weight,ncase,nbatch
+    #         self.read_data(usr)
+    #         print("!!! File ", rel_path, " is Usrxxx")
+    #         return
+    #     except Exception as e:
+    #         print(e)
+    #         print("File ", rel_path, " is not Usrxxx")
+    #     print("File ", rel_path, " cannot be opened")
+    #     return
 
-        try:
-            usr = Usrbdx(rel_path)
-            usr.sayHeader()  # file,title,time,weight,ncase,nbatch
-            usr.say()  # file,title,time,weight,ncase,nbatch
-            self.read_data(usr)
-            print("!!! File ", rel_path, " is Usrbdx")
-            return
-        except Exception as e:
-            print(e)
-            print("File ", rel_path, " is not Usrbdx")
-
-        try:
-            usr = Resnuclei(rel_path)
-            usr.sayHeader()  # file,title,time,weight,ncase,nbatch
-            usr.say()  # file,title,time,weight,ncase,nbatch
-            self.read_data(usr)
-            print("!!! File ", rel_path, " is Resnuclei")
-            return
-        except Exception as e:
-            print(e)
-            print("File ", rel_path, " is not Resnuclei")
-
-        try:
-            usr = Usrxxx(rel_path)
-            usr.sayHeader()  # file,title,time,weight,ncase,nbatch
-            self.read_data(usr)
-            print("!!! File ", rel_path, " is Usrxxx")
-            return
-        except Exception as e:
-            print(e)
-            print("File ", rel_path, " is not Usrxxx")
-        print("File ", rel_path, " cannot be opened")
-        return
-
-    def test_load_files(self):
-        # loop over all .bdo files in all subdirectories
-        for filename in os.listdir(self.main_dir):
-            rel_path = os.path.join(self.main_dir, filename)
-            print("\n\nopening", rel_path)
-            self.check(rel_path)
-
+    # def test_load_files(self):
+    #     # loop over all .bdo files in all subdirectories
+    #     for filename in os.listdir(self.main_dir):
+    #         rel_path = os.path.join(self.main_dir, filename)
+    #         print("\n\nopening", rel_path)
+    #         self.check(rel_path)
 
     def test_load_input(self):
         Input.init()
-        input = Input.Input()
-        input.read(os.path.join(self.main_dir,"proton.inp"))
-        try:
-            rndcard = input.cards["RANDOMIZ"][0]
-            rndcard.setWhat(2, 5723)
-        except:
-            print("No RANDOMIZe card found")
-        input.write("test2.inp")
-
+        for filename in os.listdir(self.main_dir):
+            rel_path = os.path.join(self.main_dir, filename)
+            if rel_path.endswith(".inp"):
+                logger.info("opening " + rel_path)
+                input = Input.Input()
+                input.read(rel_path)
+                try:
+                    rndcard = input.cards["RANDOMIZ"][0]
+                    rndcard.setWhat(2, 5723)
+                except:
+                    logger.error("No RANDOMIZe card found")
+                input.write(rel_path[:-3] + "new")
+                self.assertTrue(os.path.exists(rel_path[:-3] + "new"))
 
 if __name__ == '__main__':
     unittest.main()
