d79ef16fc7a523446189cf3bd73f55f6dcc73eba
Niels Bassler <niels.bassler@gmail.com>
2016-08-21 01:37:52 +0200

    Implement proper handling of .dos and .dosemlet.dos output, resolving issue #56

--- pymchelper/writers/trip98.py
+++ pymchelper/writers/trip98.py
@@ -8,17 +8,60 @@ class SHTripCubeWriter:
         self.output_corename = filename
 
     def write(self, detector):
-        from pytrip import dos
-        cube = dos.DosCube()
+        from pymchelper.shieldhit.detector.detector_type import SHDetType
+        
         pixel_size_x = (detector.xmax - detector.xmin) / detector.nx
         pixel_size_z = (detector.zmax - detector.zmin) / detector.nz
-        cube.create_empty_cube(
-            1.0, detector.nx, detector.ny, detector.nz, pixel_size=pixel_size_x, slice_distance=pixel_size_z)
-        cube.cube = detector.data.reshape(detector.nx, detector.ny, detector.nz)
-        if detector.tripdose >= 0.0 and detector.tripntot > 0:
-            cube.cube = (cube.cube * detector.tripntot * 1.602e-10) /\
-                detector.tripdose * 1000.0
-        else:
-            cube.cube = (cube.cube / cube.cube.max()) * 1200.0
 
-        cube.write(self.output_corename)
+        if detector.dettyp == SHDetType.dose:
+
+            from pytrip import dos
+            import numpy as np
+
+            cube = dos.DosCube()
+            cube.create_empty_cube(
+                1.0, detector.nx, detector.ny, detector.nz, pixel_size=pixel_size_x, slice_distance=pixel_size_z)
+
+            # .dos dose cubes are usually in normalized integers, where "1000" equals 100.0 % dose.
+            # the next are also the defaults, but just to be clear this is specifially set.
+            cube.data_type = "integer"
+            cube.bytes = 2
+            cube.pydata_type = np.int16
+            
+            cube.cube = detector.data.reshape(detector.nx, detector.ny, detector.nz)
+            if detector.tripdose >= 0.0 and detector.tripntot > 0:
+                cube.cube = (cube.cube * detector.tripntot * 1.602e-10) /\
+                            detector.tripdose * 1000.0
+            else:
+                cube.cube = (cube.cube / cube.cube.max()) * 1200.0
+
+            cube.write(self.output_corename)
+
+        elif detector.dettyp in (SHDetType.dlet, SHDetType.tlet, \
+                                 SHDetType.dletg, SHDetType.tletg):
+
+            from pytrip import let
+            import numpy as np
+            
+            cube = let.LETCube()
+            cube.create_empty_cube(
+                1.0, detector.nx, detector.ny, detector.nz, pixel_size=pixel_size_x, slice_distance=pixel_size_z)
+
+            # .dosemlet.dos LET cubes are usually in 32 bit floats.
+            cube.data_type = "float"
+            cube.bytes = 4
+            cube.pydata_type = np.float32
+
+            # need to redo the cube, since by default np.float32 are allocated.
+            # When https://github.com/pytrip/pytrip/issues/35 is fixed, then this should not be needed.
+            cube.cube = np.ones((cube.dimz, cube.dimy, cube.dimx), dtype=cube.pydata_type) * (1.0)
+            
+            cube.cube = detector.data.reshape(detector.nx, detector.ny, detector.nz)
+            cube.cube = cube.cube * 0.1 # MeV/cm -> keV/um
+
+            cube.write(self.output_corename)
+            
+        else:
+            import sys
+            sys.stderr.write("ERROR: can only write dose- or LET-type detectors to tripcube target.\n")
+            sys.exit(73) # Exit configuration error
