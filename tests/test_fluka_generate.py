4acf5551674dc0228f69958b94e4517ec3d510fe
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-08-20 17:07:41 +0200

    skeleton files

---
+++ tests/test_fluka_generate.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+import unittest
+import glob
+import logging
+
+import tests.res.fluka.generated.generate_input as gen
+
+logger = logging.getLogger(__name__)
+
+
+class TestFlukaGenerate(unittest.TestCase):
+    def test_create(self):
+        outdir = os.path.join("tests", "res", "fluka", "generated")
+        gen.main([outdir])
+        # for est in ("cyl", "geomap", "msh", "plane", "zone"):
+        #     logger.info("Estimator: " + est)
+        #     outfile = os.path.join(outdir, "detect_{:s}.dat".format(est))
+        #     self.assertTrue(os.path.isfile(outfile))
+
+
+if __name__ == '__main__':
+    unittest.main()
