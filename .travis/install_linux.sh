9a2c595b3ebd602089cbdd75348b18a5d8bb385b
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-10-05 18:29:46 +0200

    Cleaning requirements (#93)
    * versioneer removed
    
    * added test reqs

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -11,10 +11,5 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
 # check ubuntu version
 pip install --upgrade virtualenv$VENVVER pip$PIPVER setuptools tox wheel
 
-if [[ $TOXENV == py32 ]];
-then
-  pip install git+https://github.com/grzanka/python-versioneer.git@support_python32
-else
-  pip install --upgrade versioneer
-fi
+pip install -r tests/requirements-test.txt
 pip install -r requirements.txt
