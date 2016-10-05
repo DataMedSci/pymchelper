4e44b1c6e9a27ba2292a37e99653ddf414ec4481
Leszek Grzanka <grzanka@agh.edu.pl>
2016-10-05 16:31:38 +0200

    added test reqs

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -11,4 +11,5 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
 # check ubuntu version
 pip install --upgrade virtualenv$VENVVER pip$PIPVER setuptools tox wheel
 
+pip install -r tests/requirements-test.txt
 pip install -r requirements.txt
