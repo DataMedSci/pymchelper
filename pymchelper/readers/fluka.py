c3392ef5931bdbc943fede0b4000738325540b63
Leszek Grzanka <grzanka@agh.edu.pl>
2016-10-18 12:09:06 +0200

    missing detector title added, caused a problems with image writer

--- pymchelper/readers/fluka.py
+++ pymchelper/readers/fluka.py
@@ -54,3 +54,5 @@ class FlukaBinaryReader:
 
         # set units : detector.units are [x,y,z,v,data,detector_title]
         detector.units = [""] * 6
+
+        detector.title = usr.detector[0].name
