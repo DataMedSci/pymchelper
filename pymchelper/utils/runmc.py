6f485c0c91f8601703474525cc5328e747b82894
Leszek Grzanka <grzanka@agh.edu.pl>
2017-01-13 17:37:21 +0000

    temp runner skeleton

--- pymchelper/utils/runmc.py
+++ pymchelper/utils/runmc.py
@@ -5,6 +5,7 @@ import sys
 import argparse
 
 from pymchelper.executor import runner
+from pymchelper.writers.plots import PlotDataWriter, ImageWriter
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +38,9 @@ def main(args=sys.argv[1:]):
     parser.add_argument("input", help='input filename or directory', type=str)
     parser.add_argument('-a', '--args', help='optional arguments for MC engine', type=str, default=None)
     parser.add_argument('-e', '--executable', help='path to executable', type=str, default=None)
+    parser.add_argument('-p', '--plots', help='generate plots', type=str, default=None)
+    parser.add_argument('-b', '--binary', help='generate binary data', type=str, default=None)
+    parser.add_argument('-t', '--text', help='generate text data', type=str, default=None)
 
     parsed_args = parser.parse_args(args)
 
@@ -44,15 +48,29 @@ def main(args=sys.argv[1:]):
     if mc_args is not None and len(mc_args) > 1:
         if mc_args[0] == '[' and mc_args[-1] == ']':
             mc_args = mc_args[1:-1]
-    s = runner.Executable(parsed_args.input, parsed_args.executable, mc_args)
-    s.run()
 
-    print("Output stream:")
-    last_communicate = ""
-    while s.status != runner.Executable.FAILED_STATUS and s.status != runner.Executable.FINISHED_STATUS:
-        if s.communicate != last_communicate:
-            last_communicate = s.communicate
-            print(last_communicate)
+    if parsed_args.binary is None:
+        print("Temp exec")
+        s = runner.TempExecutor(parsed_args.input, parsed_args.executable, mc_args)
+        results = s.run()
+        for item in results:
+            print("file", item, "data", results[item])
+            if parsed_args.text:
+                writer = PlotDataWriter(item, None)
+                writer.write(results[item])
+            if parsed_args.plots:
+                writer = ImageWriter(item, None)
+                writer.write(results[item])
+    else:
+        s = runner.Executable(parsed_args.input, parsed_args.executable, mc_args)
+        s.run()
+        print("Output stream:")
+        last_communicate = ""
+        while s.status != runner.Executable.FAILED_STATUS and s.status != runner.Executable.FINISHED_STATUS:
+            if s.communicate != last_communicate:
+                last_communicate = s.communicate
+                print(last_communicate)
+
 
 
 if __name__ == '__main__':
