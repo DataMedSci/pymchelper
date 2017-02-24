65db5a8f4627c52f202877b0ead66fb78d5a7ee8
Leszek Grzanka <grzanka@agh.edu.pl>
2017-02-24 01:00:17 +0100

    aaa

--- .travis/deploy_package.sh
+++ .travis/deploy_package.sh
@@ -8,38 +8,6 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
                 # exit with a non-zero status, or zero if all commands in the
                 # pipeline exit successfully.
 
-PYPIREPO=$1
-
-write_pypirc() {
-PYPIRC=~/.pypirc
-
-if [ -e "${PYPIRC}" ]; then
-    rm ${PYPIRC}
-fi
-
-touch ${PYPIRC}
-cat <<pypirc >${PYPIRC}
-[distutils]
-index-servers =
-    pypi
-
-[pypi]
-repository: https://pypi.python.org/pypi
-username: ${PYPIUSER}
-password: ${PYPIPASS}
-pypirc
-
-if [ ! -e "${PYPIRC}" ]; then
-    echo "ERROR: Unable to write file ~/.pypirc"
-    exit 1
-fi
-}
-
-# write .pypirc file with pypi repository credentials
-set +x
-write_pypirc
-set -x
-
 # make bdist universal package
 pip install wheel
 python setup.py bdist_wheel
@@ -54,12 +22,13 @@ pip install dist/*whl
 convertmc --version
 convertmc --help
 
-# upload to pypi only if tag present
-if [[ $TRAVIS_TAG != "" ]]; then
-    pip install -U twine
-    twine --version
-    twine upload -r $PYPIREPO dist/*
-fi
-
 # make nuitka files
 ./make_single_executable.sh
+
+# prepare for shipment
+mkdir -p release_files
+cp convertmc.pyz release_files/
+
+# cleaning
+rm -rf dist
+rm -rf build
