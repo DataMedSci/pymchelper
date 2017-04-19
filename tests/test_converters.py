0d52c9fe3ebc97a20875b369ef136245f1cd4634
Niels Bassler <niels.bassler@gmail.com>
2017-04-20 01:49:20 +0200

    add some docstrings.

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -11,24 +11,32 @@ logger = logging.getLogger(__name__)
 
 class TestPld2Sobp(unittest.TestCase):
     def test_help(self):
+        """ Print help text and exit normally.
+        """
         try:
             pymchelper.utils.pld2sobp.main(["--help"])
         except SystemExit as e:
             self.assertEqual(e.code, 0)
 
     def test_version(self):
+        """ Print version and exit normally.
+        """
         try:
             pymchelper.utils.pld2sobp.main(["--version"])
         except SystemExit as e:
             self.assertEqual(e.code, 0)
 
     def test_noarg(self):
+        """ Call pld2sobp without args will cause it to fail.
+        """
         try:
             pymchelper.utils.pld2sobp.main([])
         except SystemExit as e:
             self.assertEqual(e.code, 2)
 
     def test_simple(self):
+        """ Simple conversion including diagnostic output.
+        """
         import os
         inp_path = os.path.join("tests", "res", "pld", "test.pld")
         out_path = os.path.join("tests", "res", "pld", "test.dat")
@@ -42,18 +50,24 @@ class TestPld2Sobp(unittest.TestCase):
 
 class TestTrip2Ddd(unittest.TestCase):
     def test_help(self):
+        """ Print help text and exit normally.
+        """
         try:
             pymchelper.run.main(["tripddd", "--help"])
         except SystemExit as e:
             self.assertEqual(e.code, 0)
 
     def test_version(self):
+        """ Print version and exit normally.
+        """
         try:
             pymchelper.run.main(["tripddd", "--version"])
         except SystemExit as e:
             self.assertEqual(e.code, 0)
 
     def test_noarg(self):
+        """ If pymchelper is called without arguments it should fail.
+        """
         try:
             pymchelper.run.main(["tripddd"])
         except SystemExit as e:
