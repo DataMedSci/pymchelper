cf3af081d9a72e3bfc7be4e97f484283f4af42ad
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-05-01 16:47:54 +0200

    import test

--- tests/test_call_entry.py
+++ tests/test_call_entry.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import logging
+import pymchelper
 from pymchelper import run
 from examples import generate_detect_shieldhit, generate_fluka_input
 from pymchelper.flair import Input
