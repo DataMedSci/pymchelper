577314332e05cdcef3561b630e62bbf5a5cc9af0
Leszek Grzanka <grzanka@agh.edu.pl>
2019-10-25 15:08:31 +0200

    python2 support

--- pymchelper/__init__.py
+++ pymchelper/__init__.py
@@ -1,3 +1,9 @@
+# Python2 lacks a FileNotFoundError exception, we use then IOError
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
 try:
     with open('VERSION', 'r') as f:
         __version__ = f.readline(1)
