2fc461524af8c3bb44f0e1c8896ae14da5ad4933
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-04-27 20:43:58 +0200

    version file added in manifest file, in order to include it in packaging

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as readme_file:
 setuptools.setup(
     name='pymchelper',
     version=git_version(),
-    packages=setuptools.find_packages(where='.', exclude=("*.tests", "*.tests.*", "tests.*", "tests")),
+    packages=setuptools.find_packages(where='.', exclude=("*.tests", "*.tests.*", "tests.*", "tests", "examples")),
     url='https://github.com/DataMedSci/pymchelper',
     license='MIT',
     author='Leszek Grzanka',
