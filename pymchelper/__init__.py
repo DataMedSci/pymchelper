3b24aeffb21a52c437ca163a3553cbde6ba0ec74
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-04-27 22:23:08 +0200

    python2 support

--- pymchelper/__init__.py
+++ pymchelper/__init__.py
@@ -1,6 +1,6 @@
 # find specification of the pytripgui module
-import importlib
 import os
+import sys
 
 # Python2 lacks a FileNotFoundError exception, we use then IOError
 try:
@@ -8,9 +8,18 @@ try:
 except NameError:
     FileNotFoundError = IOError
 
+PY3 = sys.version_info > (3, 0)
 # get location of __init__.py file (the one you see now) in the filesystem
-spec = importlib.util.find_spec('pymchelper')
-init_location = spec.origin
+module_name = 'pymchelper'
+if not PY3:  # Python 2
+    import imp
+    init_location = imp.find_module(module_name)[1]
+else:  # Python >= 3.4
+    import importlib
+    import importlib.util
+    spec = importlib.util.find_spec(module_name)
+    init_location = spec.origin
+
 
 # VERSION should sit next to __init__.py in the directory structure
 version_file = os.path.join(os.path.dirname(init_location), 'VERSION')
