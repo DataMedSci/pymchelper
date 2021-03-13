1aa01d5fd81155af88b9b6faf25c7a8e68f2e66d
Leszek Grzanka <grzanka@agh.edu.pl>
2021-03-13 12:05:16 +0100

    Fix for missing pages data struct in bdo2016 reader

--- pymchelper/readers/shieldhit/reader_bdo2016.py
+++ pymchelper/readers/shieldhit/reader_bdo2016.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 class SHReaderBDO2016(SHReader):
     """
     Binary format reader from version >= 0.6
+    This format doesn't have support for multiple pages per estimator
     """
     def read_data(self, estimator):
         logger.debug("Reading: " + self.filename)
@@ -206,7 +207,7 @@ class SHReaderBDO2016(SHReader):
                                                                                   estimator.geotyp)
 
             logger.debug("Done reading bdo file.")
-            logger.debug("Detector data : " + str(estimator.data))
+            logger.debug("Detector data : " + str(estimator.pages[0].data))
             logger.debug("Detector nstat: " + str(estimator.number_of_primaries))
             logger.debug("Detector nx   : " + str(estimator.x.n))
             logger.debug("Detector ny   : " + str(estimator.y.n))
