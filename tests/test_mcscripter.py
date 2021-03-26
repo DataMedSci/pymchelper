2811bc3058ec4fd26e593aaf6cd140d01a21ed27
Leszek Grzanka <grzanka@agh.edu.pl>
2021-03-26 17:36:30 +0100

    improving tests

--- tests/test_mcscripter.py
+++ tests/test_mcscripter.py
@@ -1,14 +1,27 @@
 """
 Tests for mcscripter
 """
-import unittest
 import logging
+import os
+import shutil
+import tempfile
+import unittest
+
 import pymchelper.utils.mcscripter
 
 logger = logging.getLogger(__name__)
 
 
 class TestMcScripter(unittest.TestCase):
+    def setUp(self):
+        # save location of current working directory
+        self.local_wdir = os.getcwd()
+
+    def tearDown(self):
+        # it may happen that mcscripter has changed local working directory to something else
+        # therefore we set it back to original value
+        os.chdir(self.local_wdir)
+
     def test_help(self):
         """ Print usage and exit normally.
         """
@@ -35,24 +48,27 @@ class TestMcScripter(unittest.TestCase):
 
     def test_simple(self):
         """ Simple conversion including diagnostic output.
+         TODO - mcscripter is calling os.chdir which is causing other tests to fail as they start not in the same directory
         """
         import os
         import sys
         inp_dir = os.path.join("tests", "res", "shieldhit", "mcscripter")
         inp_cfg = os.path.join(inp_dir, "test.cfg")
-        out_dir = "wdir"
+        out_dir = tempfile.mkdtemp()  # make temp working dir for output files
         try:
             pymchelper.utils.mcscripter.main([inp_cfg])
-        except AttributeError:  # on Windows with Python os.symlink is not enabled
-            self.assertEqual(os.name, 'nt')
-            self.assertEqual(sys.version_info[0], 2)
-        except SystemExit as e:
-            self.assertEqual(e.code, 0)
             self.assertTrue(os.path.isdir(out_dir))
             self.assertTrue(os.path.isdir(os.path.join(out_dir, "12C")))
             self.assertTrue(os.path.isdir(os.path.join(out_dir, "12C", "0333.100")))
             self.assertTrue(os.path.isfile(os.path.join(out_dir, "12C", "0333.100", "beam.dat")))
             self.assertTrue(os.path.islink(os.path.join(out_dir, "12C", "0333.100", "Water.txt")))
+            logger.info("Removing directory {:s}".format(out_dir))
+            shutil.rmtree(out_dir)
+        except AttributeError:  # on Windows with Python os.symlink is not enabled
+            self.assertEqual(os.name, 'nt')
+            self.assertEqual(sys.version_info[0], 2)
+            logger.info("Removing directory {:s}".format(out_dir))
+            shutil.rmtree(out_dir)
 
 
 if __name__ == '__main__':
