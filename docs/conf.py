acb536bca7b200d4837f9e49a7c3aed54cb070eb
Leszek Grzanka <grzanka@agh.edu.pl>
2021-11-24 15:44:55 +0000

    new theme

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pymchelper'
-copyright = '2016, Leszek Grzanka'
+copyright = '2021, Leszek Grzanka'
 author = 'Leszek Grzanka'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -143,7 +143,9 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+#pydata needs `pip install pydata-sphinx-theme`
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
