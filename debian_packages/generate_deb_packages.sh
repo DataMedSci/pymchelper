d5cc9879f786ae2efcb0ed865e1ce4375fbcb507
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-28 20:13:39 +0100

    better quoting

--- debian_packages/generate_deb_packages.sh
+++ debian_packages/generate_deb_packages.sh
@@ -9,30 +9,30 @@ SCRIPTS_NAMES=('convertmc' 'runmc' 'pld2sobp' 'mcscripter')
 # Github Pages has limit 100 MB, therefore instead of making a single package with all executables we create several smaller packages to fit in the limit
 for SCRIPT in "${SCRIPTS_NAMES[@]}"; do
     # copy template to a dedicated script build directory, clean it if some leftovers are present
-    rm --recursive --force pymchelper-${SCRIPT}
-    mkdir --parents pymchelper-${SCRIPT}/DEBIAN
-    cp control pymchelper-${SCRIPT}/DEBIAN/control
+    rm --recursive --force "pymchelper-${SCRIPT}"
+    mkdir --parents "pymchelper-${SCRIPT}/DEBIAN"
+    cp control "pymchelper-${SCRIPT}/DEBIAN/control"
     
     # create directory to store binary executable file
     BIN_DIR=pymchelper-${SCRIPT}/usr/bin
-    mkdir --parents ${BIN_DIR}
+    mkdir --parents "${BIN_DIR}"
 
     # download latest release of binary executable file, exit in case of failure
-    wget --quiet https://github.com/DataMedSci/pymchelper/releases/latest/download/${SCRIPT} --output-document=${BIN_DIR}/${SCRIPT} || exit 1;
-    chmod +x ${BIN_DIR}/${SCRIPT}
+    wget --quiet "https://github.com/DataMedSci/pymchelper/releases/latest/download/${SCRIPT}" --output-document="${BIN_DIR}/${SCRIPT}" || exit 1;
+    chmod +x "${BIN_DIR}/${SCRIPT}"
 
     # adjust version number
-    VERSION=$(${BIN_DIR}/${SCRIPT} --version)
+    VERSION=$("${BIN_DIR}/${SCRIPT}" --version)
     sed --in-place "s/Version\:.*/Version\: ${VERSION}/g" pymchelper-${SCRIPT}/DEBIAN/control
 
     # adjust package names
-    sed --in-place "s/Package\:.*/Package\: pymchelper-${SCRIPT}/g" pymchelper-${SCRIPT}/DEBIAN/control
-    sed --in-place "s/Description\:.*/Description\: pymchelper ${SCRIPT}/g" pymchelper-${SCRIPT}/DEBIAN/control
+    sed --in-place "s/Package\:.*/Package\: pymchelper-${SCRIPT}/g" "pymchelper-${SCRIPT}/DEBIAN/control"
+    sed --in-place "s/Description\:.*/Description\: pymchelper ${SCRIPT}/g" "pymchelper-${SCRIPT}/DEBIAN/control"
 
     # for debian <= 7 additional flags to dpkg-deb are needed: no compression and old format: -Znone --deb-format=0.939000 
 
     # for all versions use newest format of deb packages, exit in case of failure
-    dpkg-deb --root-owner-group --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}.deb || exit 1;
+    dpkg-deb --root-owner-group --build "pymchelper-${SCRIPT}" "pymchelper-${SCRIPT}.deb" || exit 1;
 done
 
 # build meta-package
