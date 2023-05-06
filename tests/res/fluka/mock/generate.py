df930651aee7099c511bb0c6d81f1303f4ddbcd8
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-05-06 13:20:29 +0200

    Use index instead name for variables

--- tests/res/fluka/mock/generate.py
+++ tests/res/fluka/mock/generate.py
@@ -13,21 +13,20 @@ import os
 import sys
 
 __FILE_NAME_AND_CONTENT_TEMPLATE = """
-FILE_NAME_{file_name_var}="{file_name}"
-FILE_NAME_CONTENT_{file_name_var}="{file_content}"
+FILE_NAME_{index}="{file_name}"
+FILE_NAME_CONTENT_{index}="{file_content}"
 sleep 1
-printf '%s' "$FILE_NAME_CONTENT_{file_name_var}" | base64 -d -i > $FILE_NAME_{file_name_var}
-echo "Generated $FILE_NAME_{file_name_var}"
+printf '%s' "$FILE_NAME_CONTENT_{index}" | base64 -d -i > $FILE_NAME_{index}
+echo "Generated $FILE_NAME_{index}"
 """
 
 __OUTPUT_SCRIPT_NAME = "rfluka"
 
 
-def to_bash_lines(file_name: str, file_content: bytes) -> str:
+def to_bash_lines(index: int, file_name: str, file_content: bytes) -> str:
     """Converts file name and content to bash lines."""
     base64_encoded_content = base64.standard_b64encode(file_content).decode("utf-8")
-    file_name_var = file_name.upper().replace(".", "_")
-    return __FILE_NAME_AND_CONTENT_TEMPLATE.format(file_name_var=file_name_var, file_name=file_name,
+    return __FILE_NAME_AND_CONTENT_TEMPLATE.format(index=index, file_name=file_name,
                                                    file_content=base64_encoded_content)
 
 
@@ -41,11 +40,11 @@ def run():
     with open(__OUTPUT_SCRIPT_NAME, "w") as f:
         f.write("#!/bin/bash\n")
 
-        for file in sorted(fluka_files):
+        for index, file in enumerate(sorted(fluka_files)):
             with open(file, 'rb') as fluka_file:
                 file_name = os.path.basename(file)
                 file_content = fluka_file.read()
-                f.write(to_bash_lines(file_name, file_content))
+                f.write(to_bash_lines(index, file_name, file_content))
     os.chmod(__OUTPUT_SCRIPT_NAME, 0o744)
 
 
