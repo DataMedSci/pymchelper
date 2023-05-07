e6333fbcaceb80fe7df72adfae733701f7c62372
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-05-07 21:14:08 +0200

    Move reusable code to separate file

--- tests/res/mocks/generate_fluka.py
+++ tests/res/mocks/generate_fluka.py
@@ -7,26 +7,9 @@ Generated bash script will contain all fluka output files as base64 encoded stri
 Running generated script will create all fluka output files in the current directory.
 """
 
-import base64
-import os
 from pathlib import Path
 
-__FILE_NAME_AND_CONTENT_TEMPLATE = """
-FILE_NAME_{index}="{file_name}"
-FILE_NAME_CONTENT_{index}="{file_content}"
-sleep 1
-printf '%s' "$FILE_NAME_CONTENT_{index}" | base64 -d -i > $FILE_NAME_{index}
-echo "Generated $FILE_NAME_{index}"
-"""
-
-__OUTPUT_SCRIPT_NAME = "rfluka"
-
-
-def to_bash_lines(index: int, file_name: str, file_content: bytes) -> str:
-    """Converts file name and content to bash lines."""
-    base64_encoded_content = base64.standard_b64encode(file_content).decode("utf-8")
-    return __FILE_NAME_AND_CONTENT_TEMPLATE.format(index=index, file_name=file_name,
-                                                   file_content=base64_encoded_content)
+from tests.res.mocks.libs.bash_script import create_script
 
 
 def run():
@@ -36,15 +19,7 @@ def run():
         print("No fluka files found in current directory.")
         return
 
-    with open(__OUTPUT_SCRIPT_NAME, "w") as f:
-        f.write("#!/bin/bash\n")
-
-        for index, file in enumerate(sorted(fluka_files)):
-            with open(file, 'rb') as fluka_file:
-                file_name = os.path.basename(file)
-                file_content = fluka_file.read()
-                f.write(to_bash_lines(index, file_name, file_content))
-    os.chmod(__OUTPUT_SCRIPT_NAME, 0o744)
+    create_script("rfluka", fluka_files)
 
 
 if __name__ == "__main__":
---
+++ tests/res/mocks/libs/bash_script.py
@@ -0,0 +1,31 @@
+import base64
+from pathlib import Path
+from typing import List
+
+__FILE_NAME_AND_CONTENT_TEMPLATE = """
+FILE_NAME_{index}="{file_name}"
+FILE_NAME_CONTENT_{index}="{file_content}"
+sleep 1
+printf '%s' "$FILE_NAME_CONTENT_{index}" | base64 -d -i > $FILE_NAME_{index}
+echo "Generated $FILE_NAME_{index}"
+"""
+
+
+def to_bash_lines(index: int, file_name: str, file_content: bytes) -> str:
+    """Converts file name and content to bash lines."""
+    base64_encoded_content = base64.standard_b64encode(file_content).decode("utf-8")
+    return __FILE_NAME_AND_CONTENT_TEMPLATE.format(index=index, file_name=file_name,
+                                                   file_content=base64_encoded_content)
+
+
+def create_script(script_name: str, files_to_save: List[Path]):
+    """Creates a bash script with given name and files to save."""
+    sp = Path(script_name)
+    with open(sp, "w") as script:
+        script.write("#!/bin/bash\n")
+        for index, file in enumerate(sorted(files_to_save)):
+            with open(file, 'rb') as f:
+                file_name = file.name
+                file_content = f.read()
+                script.write(to_bash_lines(index, file_name, file_content))
+    sp.chmod(0o744)
