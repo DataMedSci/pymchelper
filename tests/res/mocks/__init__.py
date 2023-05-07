fd852b7a638f401323f39e95b06ef53bbb6e2189
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-05-07 21:52:41 +0200

    Add unit test

---
+++ tests/res/mocks/__init__.py
@@ -0,0 +1,5 @@
+
+from pathlib import Path
+
+# Path to the directory containing this file
+PATH: Path = Path(__file__).resolve().parent
---
+++ tests/test_mocks.py
@@ -0,0 +1,53 @@
+"""Test mock scripts"""
+
+import logging
+import os
+from pathlib import Path
+from subprocess import call
+import unittest
+
+import pytest
+
+
+from pymchelper.input_output import fromfile
+from tests.res.mocks import PATH
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.skipif(os.name == 'nt', reason="Windows not supported")
+def test_fluka_mock(tmp_path: Path):
+    """Test fluka mock script"""
+    rfluka = PATH / "rfluka"
+    assert rfluka.exists(), "rfluka script does not exist"
+
+    expected_files = ["cherenkov001_fort.55", "cherenkov002_fort.55", "cherenkov003_fort.55", "cherenkov004_fort.55"]
+    env = append_path_to_environ(os.environ, PATH)
+    call("rfluka", cwd=tmp_path, env=env)
+
+    for f in expected_files:
+        fp = tmp_path / f
+        assert (tmp_path / f).exists(), f"File {f} was not created"
+        check_fluka_file(fp)
+
+
+def check_fluka_file(file: Path):
+    # creating empty Detector object and filling it with data read from Fluka file
+    fluka_data = fromfile(file)
+
+    # printing some output on the screen
+    logger.info("Fluka bins in X: {:d}, Y: {:d}, Z: {:d}".format(fluka_data.x.n, fluka_data.y.n, fluka_data.z.n))
+    logger.info("First bin of fluka data", fluka_data.pages[0].data[0, 0, 0, 0, 0])
+
+
+def append_path_to_environ(env: dict, path: Path) -> dict:
+    """Append path to PATH environment variable"""
+    if "PATH" in env:
+        env["PATH"] = str(path) + os.pathsep + env["PATH"]
+    else:
+        env["PATH"] = str(path)
+    return env
+
+
+if __name__ == '__main__':
+    unittest.main()
