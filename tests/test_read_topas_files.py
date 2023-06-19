f6971b1ebb42a1922dcf31145fffaf6d0047eef9
Mateusz Wronka <matwro2001@gmail.com>
2023-06-19 12:42:17 +0200

    test for cylinder

--- tests/test_read_topas_files.py
+++ tests/test_read_topas_files.py
@@ -7,7 +7,7 @@ from pymchelper.readers.topas import TopasReaderFactory
 
 
 def test_read():
-    file_path = os.path.join("tests", "res", "topas", "fluence_bp_protons_xy.csv")
+    file_path = os.path.join("tests", "res", "topas", "minimal", "fluence_bp_protons_xy.csv")
     
     reader = TopasReaderFactory(file_path).get_reader()
     assert reader is not None
@@ -19,8 +19,11 @@ def test_read():
     assert estimator.file_corename == "fluence_bp_protons_xy"
     assert estimator.dimension == 2
     
+    assert estimator.x.name == "X"
     assert estimator.x.n == 4
+    assert estimator.y.name == "Y"
     assert estimator.y.n == 4
+    assert estimator.z.name == "Z"
     assert estimator.z.n == 1
     
     assert estimator.pages[0].name == "FluenceBPprotonsXY"
@@ -35,3 +38,19 @@ def test_read():
                 0.00266, 0.00088, 0.00177, 0.00177,
                 0.00088, 0., 0., 0.]
     assert np.allclose(estimator.pages[0].data_raw, expected_data, atol=1e-5)
+    
+def test_cylindrical_coordinates():
+    file_path = os.path.join("tests", "res", "topas", "cylinder", "MyScorer.csv")
+    
+    reader = TopasReaderFactory(file_path).get_reader()
+    assert reader is not None
+    
+    reader = reader(file_path)
+    estimator = Estimator()
+    reader.read_data(estimator)
+    
+    assert estimator.x.name == "R"
+    assert estimator.y.name == "Phi"
+    assert estimator.z.name == "Z"
+    
+    assert estimator.pages[0].dettyp == "SurfaceTrackCount"
