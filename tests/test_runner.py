c7a5701519111d5da99ab3f1d9fd1efa13b011d1
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-06-21 22:19:07 +0200

    improved tests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,6 +3,9 @@ import sys
 from pathlib import Path
 from typing import Generator
 
+import numpy as np
+from pymchelper.input_output import fromfile
+
 import pytest
 
 from pymchelper.executor.options import SimulationSettings
@@ -32,11 +35,18 @@ END
         Geometry Cyl
             Name MyCyl
             R  0.0  10.0    1
-            Z  0.0  20.0    1000
+            Z  0.0  20.0    100
+        Filter
+            Name Primaries
+            A = 1
+            Z = 1
+            GEN = 0
         Output
             Filename data.bdo
             Geo MyCyl
             Quantity Dose
+            Quantity AvgEnergy Primaries
+            Quantity Rho
         """,
         'geo.dat':
         """*---><---><--------><------------------------------------------------>
@@ -83,3 +93,60 @@ def test_runner(example_input_cfg: dict, tmp_path: Path, monkeypatch: pytest.Mon
 
     data = r.get_data()
     assert data is not None
+
+
+@pytest.mark.smoke
+@pytest.mark.skipif(sys.platform == "darwin", reason="we don't have SHIELD-HIT12A demo binary for MacOSX")
+def test_merging(example_input_cfg: dict, tmp_path: Path, monkeypatch: pytest.MonkeyPatch, shieldhit_binary_path: Path,
+                 shieldhit_demo_binary_installed):
+    """Test if single BDO file is converted to Excel file"""
+    logging.info("Changing working directory to %s", tmp_path)
+    monkeypatch.chdir(tmp_path)
+
+    for config_file, file_content in example_input_cfg.items():
+        file_path = tmp_path / config_file
+        file_path.write_text(file_content)
+        assert file_path.exists()
+        assert file_path.is_file()
+        assert file_path.stat().st_size > 0
+
+    settings = SimulationSettings(input_path=tmp_path,
+                                  simulator_exec_path=shieldhit_binary_path,
+                                  cmdline_opts='--silent')
+    settings.set_no_of_primaries(500)
+
+    r = Runner(jobs=3, output_directory=str(tmp_path), keep_workspace_after_run=True)
+    isRunOk = r.run(settings=settings)
+    assert isRunOk
+
+    data = r.get_data()
+    assert data is not None
+    assert len(data['data_'].pages) == 3
+
+    # check if we get expected density
+    rho_page = data['data_'].pages[2]
+    assert rho_page.data.shape == (1, 1, 100, 1, 1)
+    assert rho_page.data[0, 0, 0, 0, 0] == 1.0
+    assert np.unique(rho_page.data).size == 1
+
+    # check if we get expected kinetic energy
+    ekin_page = data['data_'].pages[1]
+    assert ekin_page.data.shape == (1, 1, 100, 1, 1)
+    assert ekin_page.data[0, 0, 0, 0, 0] > 145.0
+    assert ekin_page.data[0, 0, 0, 0, 0] < 155.0
+
+    data_first_bin_dose = data['data_'].pages[0].data[0, 0, 0, 0, 0]
+
+    bdo_files = list(tmp_path.glob('run_*/data*.bdo'))
+    assert len(bdo_files) == 3
+    for bdo_file in bdo_files:
+        assert bdo_file.exists()
+        assert bdo_file.is_file()
+        assert bdo_file.stat().st_size > 0
+        bdo_data = fromfile(str(bdo_file))
+        assert bdo_data is not None
+        assert len(bdo_data.pages) == 3
+        first_bin_dose = bdo_data.pages[0].data[0, 0, 0, 0, 0]
+        # demo version of SHIELD-HIT12A cannot use different random seeds for each run,
+        # therefore the first bin dose should be the same for all runs
+        assert first_bin_dose == data_first_bin_dose
