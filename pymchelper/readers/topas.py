ca46608aa9f4fceade7a9683efd5e705d43e7442
Mateusz Wronka <matwro2001@gmail.com>
2023-06-22 12:23:29 +0200

    made suggested changes in topas reader

--- pymchelper/readers/topas.py
+++ pymchelper/readers/topas.py
@@ -1,8 +1,10 @@
-import os
+#import os
+from pathlib import Path
 import re
 
 import numpy as np
 from pymchelper.axis import MeshAxis
+from pymchelper.estimator import Estimator
 from pymchelper.page import Page
 from pymchelper.readers.common import Reader, ReaderFactory
 
@@ -22,41 +24,57 @@ class TopasReader(Reader):
 
     def __init__(self, filename):
         super(TopasReader, self).__init__(filename)
-        self.directory = os.path.dirname(filename)
+        self.directory = Path(filename).parent
 
-    def get_bins(self, dimensions, bins_data, output_data):  # skipcq: PYL-R0201
-        """Get number of bins, bin size and unit for each dimension"""
+    @staticmethod
+    def get_parameter_filename(results_data: str) -> str:
+        parameter_filename = ""
+        pattern = r"# Parameter File: (.*)"
+        match = re.search(pattern, results_data)
+        if match:
+            parameter_filename = match.group(1)
+        return parameter_filename
+
+    @staticmethod
+    def get_bins(dimensions: list[str], results_data: str) -> dict:
+        """
+        Return dict containing number of bins, bin size and unit for each dimension
+        or None if output file does not contain this information for provided dimensions
+        """
+        bins_data = {}
+        pattern = r"# {} in (\d+) bin[s ] of ([\d.]+) (\w+)"
         for dimension in dimensions:
-            pattern = f"# {dimension} in (\\d+) bin[s ] of ([\\d.]+) (\\w+)"
-            match = re.search(pattern, output_data)
+            match = re.search(pattern.format(dimension), results_data)
             if match:
                 bins_data[dimension] = {'num': int(match.group(1)),
                                         'size': float(match.group(2)),
                                         'unit': match.group(3)}
             else:
-                return False
-        return True
+                return None
+        return bins_data
 
-    def get_scorer_name(self, output_data):  # skipcq: PYL-R0201
+    @staticmethod
+    def get_scorer_name(results_data: str) -> str:
         """Get scorer name from the output file"""
         name = ""
         pattern = r"# Results for scorer: (\w+)"
-        match = re.search(pattern, output_data)
+        match = re.search(pattern, results_data)
         if match:
             name = match.group(1)
         return name
 
-    def get_scorer_unit_results(self, output_data):  # skipcq: PYL-R0201
+    @staticmethod
+    def get_scorer_unit_results(results_data: str) -> tuple[str, str, list]:
         """Get scoring quantity, unit and the scoring values (sum/mean/etc.) from the output file"""
         scorers = ['DoseToMedium', 'DoseToWater', 'DoseToMaterial', 'TrackLengthEstimator',
                    'AmbientDoseEquivalent', 'EnergyDeposit', 'Fluence', 'EnergyFluence',
                    'StepCount', 'OpticalPhotonCount', 'OriginCount', 'Charge', 'EffectiveCharge',
                    'ProtonLET', 'SurfaceCurrent', 'SurfaceTrackCount', 'PhaseSpace']
         for scorer in scorers:
-            if scorer in output_data:
+            if scorer in results_data:
                 unit = ""
                 pattern = f"# {scorer}\\s(\\( .*? \\))?\\s*: (.*)"
-                match = re.search(pattern, output_data)
+                match = re.search(pattern, results_data)
                 if match:
                     unit = ""
                     if match.group(1):
@@ -65,11 +83,12 @@ class TopasReader(Reader):
                     return scorer, unit, results
         return "", "", []
 
-    def get_differential_axis(self, output_data):  # skipcq: PYL-R0201
+    @staticmethod
+    def get_differential_axis(results_data: str) -> MeshAxis:
         """Check if the output file contains differential axis and get it from file if it does"""
-        if "# Binned by" in output_data:
+        if "# Binned by" in results_data:
             pattern = r"# Binned by (.+?) in (\d+) bin[s ] of (\d+) (\w+) from ([\d.]+) (\w+) to ([\d.]+) (\w+)"
-            match = re.search(pattern, output_data)
+            match = re.search(pattern, results_data)
             if match:
                 binned_by = match.group(1)
                 num_bins = int(match.group(2))
@@ -81,20 +100,21 @@ class TopasReader(Reader):
                                 name=binned_by, unit=unit, binning=MeshAxis.BinningType.linear)
         return None
 
-    def read_data(self, estimator):
+    def read_data(self, estimator: Estimator) -> bool:
         """
         Read the data from the file and store them in the provided estimator object.
         Topas reader assumes that the input file is in the same directory as the output file
         to extract the number of histories from it - otherwise the number of histories is set to 0.
         """
-        # find the input file to extract the number of histories from it
-        num_histories = 0
-        for input_file in os.listdir(self.directory):
-            if input_file.endswith(".txt"):
-                input_file_path = os.path.join(self.directory, input_file)
-                with open(input_file_path) as input_file:
+        with open(self.filename, 'r') as results_file:
+            results_data = results_file.read()
+
+            num_histories = 0
+            input_filename = TopasReader.get_parameter_filename(results_data)
+            input_file_path = Path(self.directory)/ input_filename
+            if input_file_path.exists():
+                with open(input_file_path, 'r') as input_file:
                     pattern = r'NumberOfHistoriesInRun\s*=\s*(\d+)'
-                    num_histories = 0
                     input_data = input_file.read()
                     match = re.search(pattern, input_data)
                     if match:
@@ -102,21 +122,20 @@ class TopasReader(Reader):
                         if number_str:
                             num_histories = int(number_str.group())
 
-        # generate estimator object for each output file
-        with open(self.filename) as output_file:
-            output_data = output_file.read()
-
-            bins_data = {}
             dimensions = [['X', 'Y', 'Z'],
                           ['R', 'Phi', 'Z'],
                           ['Rho', 'Phi', 'Theta']]
 
             for curr_dimensions in dimensions:
-                if self.get_bins(curr_dimensions, bins_data, output_data):
+                bins_data = TopasReader.get_bins(curr_dimensions, results_data)
+                if bins_data is not None:
                     actual_dimensions = curr_dimensions
                     break
+            
+            if bins_data is None:
+                return False
 
-            estimator.file_corename = os.path.basename(self.filename)[:-4]
+            estimator.file_corename = Path(self.filename).name[:-4]
             estimator.number_of_primaries = num_histories
             estimator.file_format = "csv"
             x_max = bins_data[actual_dimensions[0]]['size']*bins_data[actual_dimensions[0]]['num']
@@ -135,11 +154,11 @@ class TopasReader(Reader):
                                    name=actual_dimensions[2], unit=bins_data[actual_dimensions[2]]['unit'],
                                    binning=MeshAxis.BinningType.linear)
 
-            differential_axis = self.get_differential_axis(output_data)
+            differential_axis = TopasReader.get_differential_axis(results_data)
 
             # in one output csv file there can be multiple results for one scorer
             # (e.g. sum and mean for fluence)
-            scorer, unit, results = self.get_scorer_unit_results(output_data)
+            scorer, unit, results = TopasReader.get_scorer_unit_results(results_data)
             num_results = len(results)
             for column, result in enumerate(results):
                 page = Page(estimator=estimator)
@@ -157,7 +176,7 @@ class TopasReader(Reader):
                     lines = np.genfromtxt(self.filename, delimiter=',')
                     scores = lines[:, column+3]
 
-                page.title = self.get_scorer_name(output_data)
+                page.title = TopasReader.get_scorer_name(results_data)
                 page.name = page.title
                 page.dettyp, page.unit = scorer+result, unit
 
@@ -168,5 +187,5 @@ class TopasReader(Reader):
             return True
 
     @property
-    def corename(self):
+    def corename(self) -> str:
         return self.filename[:-4]
