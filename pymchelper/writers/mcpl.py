4211b9fd9c9d0bd67affec664ec804a46fbf71c7
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-06-26 22:48:21 +0200

    faster writing using numpy

--- pymchelper/writers/mcpl.py
+++ pymchelper/writers/mcpl.py
@@ -1,6 +1,8 @@
 import logging
 from pathlib import Path
 import struct
+
+import numpy as np
 import pymchelper
 
 from pymchelper.page import Page
@@ -50,37 +52,80 @@ class MCPLWriter(Writer):
             # iterate over rows in the data array page.data
             # need to fix the structure according to MCPL format
             # see https://mctools.github.io/mcpl/mcpl.pdf#nameddest=section.3
-            for row in page.data.T:
-                pdg, x, y, z, ux, uy, uz, E = row
 
-                # adaptive projection packing
-                fp1: float = float('nan')
-                fp2: float = float('nan')
-                sign: int = 1
-                if ux * ux > uy * uy and ux * ux > uz * uz:
-                    if ux < 0:
-                        sign = -1
-                    fp1 = 1 / uz
-                    fp2 = uy
-                if uy * uy > ux * ux and uy * uy > uz * uz:
-                    if uy < 0:
-                        sign = -1
-                    fp1 = ux
-                    fp2 = 1 / uz
-                if uz * uz >= ux * ux and uz * uz >= uy * uy:
-                    if uz < 0:
-                        sign = -1
-                    fp1 = ux
-                    fp2 = uy
+            print(page.data.shape)
+            pdg = page.data[0]
+            x = page.data[1]
+            y = page.data[2]
+            z = page.data[3]
+            ux = page.data[4]
+            uy = page.data[5]
+            uz = page.data[6]
+            E = page.data[7]
+            fp1 = np.empty_like(ux)
+            fp2 = np.empty_like(uy)
+            sign = np.ones_like(x, dtype=int)
 
-                bytes_to_write += struct.pack("<f", x)  # x
-                bytes_to_write += struct.pack("<f", y)  # y
-                bytes_to_write += struct.pack("<f", z)  # z
-                bytes_to_write += struct.pack("<f", fp1)  # FP1 (mostly ux)
-                bytes_to_write += struct.pack("<f", fp2)  # FP2 (mostly uy)
-                bytes_to_write += struct.pack("<f", sign * E)  # uz
-                bytes_to_write += struct.pack("<f", 0)  # time
-                bytes_to_write += struct.pack("<I", int(pdg))  # pdg
+            condition_1 = np.logical_and(np.logical_and(ux * ux > uy * uy, ux * ux > uz * uz), ux < 0)
+            condition_2 = np.logical_and(np.logical_and(uy * uy > ux * ux, uy * uy > uz * uz), uy < 0)
+            condition_3 = np.logical_and(np.logical_and(uz * uz >= ux * ux, uz * uz >= uy * uy), uz < 0)
+
+            sign[ux < 0] = -1
+            fp1[condition_1] = 1 / uz[condition_1]
+            fp2[condition_1] = uy[condition_1]
+
+            sign[uy < 0] = -1
+            fp1[condition_2] = ux[condition_2]
+            fp2[condition_2] = 1 / uz[condition_2]
+
+            sign[uz < 0] = -1
+            fp1[condition_3] = ux[condition_3]
+            fp2[condition_3] = uy[condition_3]
+
+            bytes_to_write = np.empty(page.data.shape, dtype=np.float32)
+            bytes_to_write[0] = x
+            bytes_to_write[1] = y
+            bytes_to_write[2] = z
+            bytes_to_write[3] = fp1
+            bytes_to_write[4] = fp2
+            bytes_to_write[5] = sign * E
+            bytes_to_write[6] = 0
+            bytes_to_write[7] = pdg.astype(np.uint32)
+
+            bytes_to_write = bytes_to_write.tobytes()
+
+            # for i, row in enumerate(page.data.T):
+
+            #     pdg, x, y, z, ux, uy, uz, E = row
+
+            #     # adaptive projection packing
+            #     fp1: float = float('nan')
+            #     fp2: float = float('nan')
+            #     sign: int = 1
+            #     if ux * ux > uy * uy and ux * ux > uz * uz:
+            #         if ux < 0:
+            #             sign = -1
+            #         fp1 = 1 / uz
+            #         fp2 = uy
+            #     if uy * uy > ux * ux and uy * uy > uz * uz:
+            #         if uy < 0:
+            #             sign = -1
+            #         fp1 = ux
+            #         fp2 = 1 / uz
+            #     if uz * uz >= ux * ux and uz * uz >= uy * uy:
+            #         if uz < 0:
+            #             sign = -1
+            #         fp1 = ux
+            #         fp2 = uy
+
+            #     bytes_to_write += struct.pack("<f", x)  # x
+            #     bytes_to_write += struct.pack("<f", y)  # y
+            #     bytes_to_write += struct.pack("<f", z)  # z
+            #     bytes_to_write += struct.pack("<f", fp1)  # FP1 (mostly ux)
+            #     bytes_to_write += struct.pack("<f", fp2)  # FP2 (mostly uy)
+            #     bytes_to_write += struct.pack("<f", sign * E)  # uz
+            #     bytes_to_write += struct.pack("<f", 0)  # time
+            #     bytes_to_write += struct.pack("<I", int(pdg))  # pdg
 
             output_path.write_bytes(bytes_to_write)
             return
