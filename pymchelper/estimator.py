fix number_of_primaries calculation

pymchelper/estimator.py
@@ -80,9 +80,9 @@ class Estimator:
         """
         new_page = copy.deepcopy(page)
         new_page.estimator = self
-        self.pages += (new_page,)
+        self.pages += (new_page, )
 
-    def axis(self, axis_id : int) -> Optional[MeshAxis]:
+    def axis(self, axis_id: int) -> Optional[MeshAxis]:
         """
         Mesh axis selector method based on integer id's.
 
@@ -133,6 +133,7 @@ def average_with_nan(estimator_list, error_estimate=ErrorEstimate.stderr):
     if not estimator_list:
         return None
     result = copy.deepcopy(estimator_list[0])
+    result.number_of_primaries = sum([estimator.number_of_primaries for estimator in estimator_list])
     for page_no, page in enumerate(result.pages):
         page.data_raw = np.nanmean([estimator.pages[page_no].data_raw for estimator in estimator_list], axis=0)
     result.file_counter = len(estimator_list)
@@ -141,7 +142,8 @@ def average_with_nan(estimator_list, error_estimate=ErrorEstimate.stderr):
         # s : corrected sample standard deviation
         for page_no, page in enumerate(result.pages):
             page.error_raw = np.nanstd([estimator.pages[page_no].data_raw for estimator in estimator_list],
-                                       axis=0, ddof=1)
+                                       axis=0,
+                                       ddof=1)
 
         # if user requested standard error then we calculate it as:
         # S = stderr = stddev / sqrt(n), or in other words,