beb8779e444b7a870e98b682a6ddf372a2281290
Leszek Grzanka <leszek.grzanka@gmail.com>
2024-04-18 08:39:59 +0000

    deepsource

--- setup.py
+++ setup.py
@@ -78,8 +78,8 @@ EXTRAS_REQUIRE['full'].extend(["hipsterplot", "bashplotlib"])  # these are neede
 # |---------------------------------------------------|
 # see https://www.python.org/dev/peps/pep-0508/ for language specification
 install_requires = [
-    "numpy>=1.26 ; python_version == '3.12'", 
-    "numpy>=1.23.3 ; python_version == '3.11'", 
+    "numpy>=1.26 ; python_version == '3.12'",
+    "numpy>=1.23.3 ; python_version == '3.11'",
     "numpy>=1.21 ; python_version == '3.10'",
     "numpy>=1.20,<1.27.0 ; python_version == '3.9'"
 ]
