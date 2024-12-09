73924b9c83d4f2ba20dbac9714a282c6c643d554
Leszek Grzanka <leszek.grzanka@gmail.com>
2024-12-09 22:34:53 +0100

    test

--- debian_packages/add_packages_to_repo.sh
+++ debian_packages/add_packages_to_repo.sh
@@ -7,9 +7,11 @@ set -x
 ./tools/aptly repo create -distribution="stable" -component="main" main
 ./tools/aptly repo add main pymchelper-convertmc.deb
 ./tools/aptly repo add main pymchelper-runmc.deb
-./tools/aptly repo add main pymchelper-plan2sobp.deb
+#./tools/aptly repo add main pymchelper-plan2sobp.deb  # skipping as its exceeding the 100MB limit
 ./tools/aptly repo add main pymchelper-mcscripter.deb
 ./tools/aptly repo add main pymchelper.deb
+gpg --list-secret-keys
+gpg --list-keys --keyid-format LONG
 ./tools/aptly publish repo -batch main
 
 ls -alh ~/.aptly/public
