5c203b3b2c5058809a748e457b57f33c3470d16a
Leszek Grzanka <leszek.grzanka@gmail.com>
2024-12-09 14:59:26 +0100

    bump SH12A version in tests (#776)

--- docs/images_generation/run.sh
+++ docs/images_generation/run.sh
@@ -9,7 +9,7 @@ rm -f shieldhit
 rm -rf shieldhit_demo.tar.gz
 
 # fetch demo version of SHIELD-HIT12A code from main project page (extra --user-agent needed to run the code on github actions)
-wget -d --user-agent="Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0" https://shieldhit.org/download/DEMO/shield_hit12a_x86_64_demo_gfortran_v1.0.1.tar.gz -O shieldhit_demo.tar.gz
+wget -d --user-agent="Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0" https://shieldhit.org/download/DEMO/shield_hit12a_x86_64_demo_gfortran_v1.1.0.tar.gz -O shieldhit_demo.tar.gz
 
 # unpack the SHIELD-HIT12A package and extract shieldhit binary to current directory
 tar -zxvf shieldhit_demo.tar.gz
