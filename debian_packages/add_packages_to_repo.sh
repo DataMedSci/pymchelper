5363a02c59825fb37bbaf0b8bfcc149716a6e0fa
Leszek Grzanka <leszek.grzanka@gmail.com>
2024-12-10 10:53:50 +0100

    improve deb package production (#781)

--- debian_packages/add_packages_to_repo.sh
+++ debian_packages/add_packages_to_repo.sh
@@ -3,15 +3,18 @@
 # Print commands and their arguments as they are executed
 set -x
 
+./tools/aptly version
 ./tools/aptly repo create -distribution="stable" -component="main" main
 ./tools/aptly repo add main pymchelper-convertmc.deb
 ./tools/aptly repo add main pymchelper-runmc.deb
-./tools/aptly repo add main pymchelper-plan2sobp.deb
+#./tools/aptly repo add main pymchelper-plan2sobp.deb  # skipping as its exceeding the 100MB limit
 ./tools/aptly repo add main pymchelper-mcscripter.deb
 ./tools/aptly repo add main pymchelper.deb
-./tools/aptly publish repo -batch main
+gpg --list-keys --keyid-format LONG
 
-ls -alh ~/.aptly/public      
+./tools/aptly publish repo main
+
+ls -alh ~/.aptly/public
 
 mv ~/.aptly/public .
 
