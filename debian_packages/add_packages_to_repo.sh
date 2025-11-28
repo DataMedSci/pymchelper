6d56e119a07c785b86dd52c6a88f657cceb147ea
Leszek Grzanka <leszek.grzanka@gmail.com>
2025-11-28 08:56:15 +0100

    Fix repository upload workflow and update package addition script

--- debian_packages/add_packages_to_repo.sh
+++ debian_packages/add_packages_to_repo.sh
@@ -6,15 +6,17 @@ set -x
 ./tools/aptly version
 ./tools/aptly repo create -distribution="stable" -component="main" main
 ./tools/aptly repo add main pymchelper-convertmc.deb
-./tools/aptly repo add main pymchelper-runmc.deb
+#./tools/aptly repo add main pymchelper-runmc.deb
 #./tools/aptly repo add main pymchelper-plan2sobp.deb  # skipping as its exceeding the 100MB limit
-./tools/aptly repo add main pymchelper-mcscripter.deb
+#./tools/aptly repo add main pymchelper-mcscripter.deb
 ./tools/aptly repo add main pymchelper.deb
 gpg --list-keys --keyid-format LONG
 
 ./tools/aptly publish repo main
 
 ls -alh ~/.aptly/public
+# check total size of the repo
+du -sh ~/.aptly/public
 
 mv ~/.aptly/public .
 
